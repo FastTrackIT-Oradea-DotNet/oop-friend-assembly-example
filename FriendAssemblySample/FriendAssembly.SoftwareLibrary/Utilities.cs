e379d552e962ecd63aa8c460e736100b09c85dd0
Florin Lazar <florin.lazar.it2014@gmail.com>
2020-03-05 16:46:11 +0200

    Sample project for friend assemblies

---
+++ FriendAssemblySample/FriendAssembly.SoftwareLibrary/Utilities.cs
@@ -0,0 +1,32 @@
+﻿namespace FriendAssembly.SoftwareLibrary
+{
+    public static class Utilities
+    {
+        public static int[] ConvertToIntArray(string[] stringArray)
+        {
+            int[] result = new int[stringArray?.Length ?? 0];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = ConvertStringToInt(stringArray?[i]);
+            }
+
+            return result;
+        }
+
+        internal static int ConvertStringToInt(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return 0;
+            }
+
+            if (int.TryParse(value, out int result))
+            {
+                return result;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ FriendAssemblySample/FriendAssembly.UnitTests/UtilitiesTests.cs
@@ -0,0 +1,51 @@
+﻿using FriendAssembly.SoftwareLibrary;
+using Xunit;
+
+namespace FriendAssembly.UnitTests
+{
+    public class UtilitiesTests
+    {
+        [Fact]
+        public void ConvertToIntArray_Should_Return_Empty_Array_When_Input_Is_Null()
+        {
+            int[] result = Utilities.ConvertToIntArray(null);
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void ConvertToIntArray_Should_Return_Empty_Array_When_Input_Is_Empty()
+        {
+            int[] result = Utilities.ConvertToIntArray(new string[0]);
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void ConvertToIntArray_Should_Return_Empty_Array_When_Input_Has_Value()
+        {
+            int[] result = Utilities.ConvertToIntArray(new string[] { "123", "456" });
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Length);
+            Assert.Equal(123, result[0]);
+            Assert.Equal(456, result[1]);
+        }
+
+        [Theory]
+        [InlineData("123", 123)]
+        [InlineData("-123", -123)]
+        [InlineData(null, 0)]
+        [InlineData("", 0)]
+        [InlineData("    ", 0)]
+        [InlineData("abc", 0)]
+        public void ConvertToIntArray_StringConversions_Tests(string value, int expected)
+        {
+            int result = Utilities.ConvertStringToInt(value);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
